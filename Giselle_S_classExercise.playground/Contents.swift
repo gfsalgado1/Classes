0595add30216af25202cc54ae19550b8fe30396f
gfsalgado1 <52936092+gfsalgado1@users.noreply.github.com>
2020-07-27 14:05:45 -0500

    Add files via upload

---
+++ Giselle_S_classExercise.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class carFeatures{
+    var mileage: Int = 70000
+    var color: String = "Black"
+    var engine: String = "V4"
+    var year: Int = 2017
+}
+
+let features = carFeatures()
+print("The 4 more important features for when I get a car is that: \n")
+print("The milage of the car is \(features.mileage)")
+print("and the color of the car is \(features.color)")
+print("also the year of the car is \(features.year) and the engine of the car is \(features.engine).")
+
